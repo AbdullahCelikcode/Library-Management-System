8cabbaf6c7d8e8c511b7b4ede80c180bca111aea
Abdullah Çelik <celika80@hotmail.com>
2023-10-14 01:21:01 +0300

    Adding Entity

--- src/main/java/Library/LibraryManagement/entity/Admin.java
+++ src/main/java/Library/LibraryManagement/entity/Admin.java
@@ -1,4 +1,8 @@
 package Library.LibraryManagement.entity;
 
 public class Admin {
+    private int id;
+    private String name;
+    private String surName;
+
 }
--- src/main/java/Library/LibraryManagement/entity/Category.java
+++ src/main/java/Library/LibraryManagement/entity/Category.java
@@ -1,4 +1,24 @@
 package Library.LibraryManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "category")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
 }
